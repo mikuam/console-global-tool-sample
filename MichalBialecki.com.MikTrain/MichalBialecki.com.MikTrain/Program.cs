fdc9076b3bcd03f03fa4d1d61e219f322e81100f
Michal Bialecki <michal.bialecki@fabres.pl>
2019-09-30 14:31:59 +0200

    [minor] Added a simple console animation

--- MichalBialecki.com.MikTrain/MichalBialecki.com.MikTrain/Program.cs
+++ MichalBialecki.com.MikTrain/MichalBialecki.com.MikTrain/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace MichalBialecki.com.MikTrain
 {
@@ -6,7 +7,59 @@ namespace MichalBialecki.com.MikTrain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+            MultiLineAnimation();
+
+            Console.ReadKey();
+        }
+
+        static void MultiLineAnimation()
+        {
+            var counter = 0;
+            for (int i = 0; i < 30; i++)
+            {
+                Console.Clear();
+
+                switch (counter % 4)
+                {
+                    case 0:
+                    {
+                        Console.WriteLine("╔════╤╤╤╤════╗");
+                        Console.WriteLine("║    │││ \\   ║");
+                        Console.WriteLine("║    │││  O  ║");
+                        Console.WriteLine("║    OOO     ║");
+                        break;
+                    };
+                    case 1:
+                    {
+                        Console.WriteLine("╔════╤╤╤╤════╗");
+                        Console.WriteLine("║    ││││    ║");
+                        Console.WriteLine("║    ││││    ║");
+                        Console.WriteLine("║    OOOO    ║");
+                        break;
+                    };
+                    case 2:
+                    {
+                        Console.WriteLine("╔════╤╤╤╤════╗");
+                        Console.WriteLine("║   / │││    ║");
+                        Console.WriteLine("║  O  │││    ║");
+                        Console.WriteLine("║     OOO    ║");
+                        break;
+                    };
+                    case 3:
+                    {
+                        Console.WriteLine("╔════╤╤╤╤════╗");
+                        Console.WriteLine("║    ││││    ║");
+                        Console.WriteLine("║    ││││    ║");
+                        Console.WriteLine("║    OOOO    ║");
+                        break;
+                    };
+                }
+
+                counter++;
+                Thread.Sleep(200);
+            }
         }
     }
 }
